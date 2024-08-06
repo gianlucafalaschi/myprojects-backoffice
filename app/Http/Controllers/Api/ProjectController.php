5fe66526f5b0267a09aad67b110bbbce34bc836d
gianlucafalaschi <gian100gian@outlook.it>
2024-08-06 17:59:33 +0200

    add:  projects index api now gives paginated results

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index() {
 
-        $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->paginate(6);
         //dd($projects);
 
         return response()->json([
