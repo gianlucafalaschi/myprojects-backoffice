2c9da8c360a8dc5d6fa9a396fde5e030a0e8e297
gianlucafalaschi <gian100gian@outlook.it>
2024-09-13 17:05:23 +0200

    add: description paragraph

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -3,10 +3,12 @@
 @section('content')
     <h1>All Technologies</h1>
 
+    <p>Choose a technology to see all the projects that utilize it.</p>
+
     @foreach ($technologies as $technology)
     <ul>
         <li>
-            <a href="{{route('admin.technologies.show', ['technology'=> $technology->slug]) }}">{{$technology->name}}</a>
+            <a href="{{route('admin.technologies.show', ['technology'=> $technology->slug]) }}" class="text-decoration-none">{{$technology->name}}</a>
         </li>
     </ul> 
     @endforeach
